a4056a0d8cd5e2ad7b62693b1443d7ae94bccf63
shelbyclarice <shelbyclarice@gmail.com>
2023-02-26 23:05:21 -0500

    week 5

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+/* Each box will have equation for other boxes*/
 let celciusInput = document.querySelector("#celcius > input");
 let fahrenheitInput = document.querySelector("#fahrenheit > input");
 let kelvinInput = document.querySelector("#kelvin > input");
 
+/* All Clear button */
 let btn = document.querySelector(".button button");
 
+/* Round the number */
 function roundNumber(number) {
   return Math.round(number * 100) / 100;
 }
 
+/* Celcius to Fahrenheit and Kelvin*/
 celciusInput.addEventListener("input", function () {
   let cTemp = parseFloat(celciusInput.value);
   let fTemp = cTemp * (9 / 5) + 32;
@@ -17,6 +21,7 @@ celciusInput.addEventListener("input", function () {
   kelvinInput.value = roundNumber(kTemp);
 });
 
+/* Fahrenheit to Celcius and Kelvin */
 fahrenheitInput.addEventListener("input", function () {
   let fTemp = parseFloat(fahrenheitInput.value);
   let cTemp = (fTemp - 32) * (5 / 9);
@@ -26,6 +31,7 @@ fahrenheitInput.addEventListener("input", function () {
   kelvinInput.value = roundNumber(kTemp);
 });
 
+/* Kelvin to Celcius and Fahrenheit */
 kelvinInput.addEventListener("input", function () {
   let kTemp = parseFloat(kelvinInput.value);
   let cTemp = kTemp - 273.15;
@@ -35,6 +41,7 @@ kelvinInput.addEventListener("input", function () {
   fahrenheitInput.value = roundNumber(fTemp);
 });
 
+/*All Clear Button to clear numbers*/
 btn.addEventListener("click", () => {
   celciusInput.value = "";
   fahrenheitInput.value = "";
